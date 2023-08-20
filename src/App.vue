cdeadc28be27741abd2d2b69aac165e0f8c84943
Arhan Islam <arhanislam3407@gmail.com>
2023-08-20 21:09:22 +0600

    Assignment for Module-08

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+import ParentComponent from './components/ParentComponent.vue';
+</script>
+<template>
+<ParentComponent/>
+</template>
+<style scoped>
+</style>
---
+++ src/components/ParentComponent.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref} from 'vue'
+import ChildComponent from './ChildComponent.vue';
+const person = {
+    name:'Ashraful Islam',
+    title:'I can ride a Track Marlin 7',
+    url:'https://www.trekbikes.com/us/en_US/'
+}
+
+const messageToChild = 'Welcome to Child Component'
+const messageFromChild = ref('')
+</script>
+<template>
+    <div class="h-screen bg-gradient-to-r flex items-center justify-center flex-col">
+        <h1 class="font-bold ">{{ messageFromChild }}</h1>
+        <ChildComponent :person = person :messageToChild="messageToChild" v-model:messageFromChild="messageFromChild"></ChildComponent>
+    </div>
+</template>
